94435ab630cf5944188aa15e015bb96bc0dad4a1
Vitor <vitorbukovitz@hotmai.com>
2020-02-13 22:51:45 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:giphy_app/ui/home_page.dart';
+import 'package:giphy_app/ui/gif_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+
